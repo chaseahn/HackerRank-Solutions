978c5e030c952cddef87ffcfe1ad0ff1c88c07b5
Chase Ahn <ahn.ch@Chases-MacBook-Pro.local>
2019-02-26 17:05:11 -0500

    add counting valleys

---
+++ counting_valleys.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+'''
+Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike he took exactly n steps. 
+For every step he took, he noted if it was an uphill, U , or a downhill, D step. Gary's hikes start and end at sea level and each step up or down represents 
+a 1 unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+
+For example, if Gary's path is s=[DDUUUUDD], he first enters a valley 2 units deep. Then he climbs out an up onto a mountain 2 units high. 
+Finally, he returns to sea level and ends his hike.
+
+Function Description
+
+Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+'''
+
+import sys
+
+def countingValleys(n, s):
+
+    height = 0
+    vallies = 0
+
+    for step in s: 
+        if step == 'U':
+            height += 1
+        else:
+            height -= 1
+    
+        if height == 0 and step == 'U':
+            vallies += 1
+
+    return vallies
